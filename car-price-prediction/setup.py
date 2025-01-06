371e499eee7d578df35de81c9348bc8bdb7e98a4
abhishek7260 <abhishekranjan7260@gmail.com>
2025-01-06 10:08:13 +0530

    initial commit

---
+++ car-price-prediction/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this model is used to predict the price of car',
+    author='abhishek7260',
+    license='MIT',
+)
